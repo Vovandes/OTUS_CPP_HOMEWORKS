c92e568feb1428ddcd909ef5804744d163e887b1
WIN-M6PH0EA6RAS\VoS <akms@bk.ru>
2022-07-03 22:08:20 +0300

    Добавьте файлы проекта.

---
+++ CMakeProject1_Hello_World/hello_world.cpp
@@ -0,0 +1,17 @@
+#include "example.h"
+
+#include <iostream>
+
+void todo_move_me_out() {
+	std::cout << "This function should be located in the separated source file" << std::endl;
+}
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+
+	example_function();
+	todo_move_me_out();
+	
+	system("pause");
+	return 0;
+}
